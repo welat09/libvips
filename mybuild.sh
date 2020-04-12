16492f5509aac307e72c9b0b8da5364683b3c136
welat09 <44067166+welat09@users.noreply.github.com>
2020-04-12 12:52:18 +0200

    Create mybuild.sh

---
+++ mybuild.sh
@@ -0,0 +1,2 @@
+./configure --enable-static --without-magick --without-lcms  --without-OpenEXR --without-nifti --without-pdfium --without-rsvg --without-matio --without-libwebp --without-cfitsio --without-zlib --without-poppler --without-pangoft2 --enable-introspection=no --without-openslide
+make -j 8
